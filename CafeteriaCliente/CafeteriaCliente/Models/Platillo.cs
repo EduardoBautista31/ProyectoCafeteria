8e23f79a8e45166c423f4be1dcb752c8addeba4b
bauti <bauti@DESKTOP-BV2C7C2>
2022-11-23 13:42:01 -0600

    Avance 2

---
+++ CafeteriaCliente/CafeteriaCliente/Models/Platillo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeteriaServidor.Models
+{
+    public class Platillo
+    {
+        public int NumeroMesa { get; set; }
+        public string NombrePlatillo { get; set; } = "";
+        public decimal CantidadPlatillos { get; set; }
+    }
+}
