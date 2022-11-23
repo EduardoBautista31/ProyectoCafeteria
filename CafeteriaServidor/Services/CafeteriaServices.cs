dc2e2cc85f17f327dbfb77da9919db85e573ce12
bauti <bauti@DESKTOP-BV2C7C2>
2022-11-23 16:04:56 -0600

    Avances 3

---
+++ CafeteriaServidor/Services/CafeteriaServices.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeteriaServidor.Services
+{
+    public class CafeteriaServices
+    {
+        //ESTE ES EL SERVIDOR
+        HttpListener listener = new HttpListener();
+        public CafeteriaServices()
+        {
+            listener.Prefixes.Add("http://*:12056/pedidos");
+
+        }
+        public void Iniciar()
+        {
+            if (!listener.IsListening)
+            {
+                listener.Start();
+                listener.BeginGetContext(ContextoRecibido, null);
+            }
+
+        }
+
+        private void ContextoRecibido(IAsyncResult ar)
+        {
+            var context = listener.EndGetContext(ar);
+
+        }
+
+    }
+}
